bbe7501b0940a33fa9f6d0fec461987a7b336a29
Rajdeep Biswas <32306614+therajdeepbiswas@users.noreply.github.com>
2020-04-19 17:50:45 +0530

    updated

--- 2020/pset6/dna.py
+++ 2020/pset6/dna.py
@@ -1,35 +1,31 @@
 import sys
 import pandas as pd
+import re
 
-# check for the correct number CL arguments
 if len(sys.argv) != 3:
 	print("Usage: python dna.py data.csv sequence.txt")
 	exit()
 
-# fetching the csv and txt filepaths
 [csvpath, seqpath] = sys.argv[1:]
 
-# reading from the csv and txt and storing them in variables
 df = pd.read_csv(csvpath)
 seq = ""
 with open(seqpath, "r") as f:
 	for line in f:
 		seq += line
 
-# getting each of the csv headers (the STRs) except the 0th one (the name of person)
-dnathings = [thing for thing in df][1:]
+strs = [st for st in df][1:]
 
-# initializing output to a "No match"
 name = "No match"
 
-# iterating through each csvrow
 for index, row in df.iterrows():
+	if False not in [st * int(row[[st]]) in seq for st in strs]:
 
-	# checking if each STR sequence repeated n number of times, appears in the specified text file
-	# (where n is each person's STR occurrence)
-	# an all 'True' list would mean complete appearance of each STR
-	if False not in [item * int(row[[item]]) in seq for item in dnathings]:
-		name = row["name"]
-		break
+		truth = []
+		for st in strs:
+			truth.append(seq[seq.find(st * int(row[st])) + len(st * (int(row[st]))):][:len(st)] == st)
+
+		if True not in truth:
+			name = row["name"]
 
 print(name)
